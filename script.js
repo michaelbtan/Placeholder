8fc86d5f8ac9392348d8c596b355f50a6fc4bbc9
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2021-11-09 11:44:25 -0500

    adding to the css, and capitalized the first letter of the entry

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 async function getPokemon(pokemonName) {
   const url = `https://api.pokemontcg.io/v2/cards?q=name:${pokemonName}`
-  console.log(url)
+  // console.log(url)
   try {
     const res = await axios.get(url)
-    console.log(res)
+    // console.log(res)
     const cardData = await res.data.data
     renderPokemon(cardData);
-    console.log(cardData)
+    // console.log(cardData)
   }
   catch (error) {
     console.log(error)
@@ -17,26 +17,38 @@ async function getPokemon(pokemonName) {
 const userInput = document.querySelector("#blank");
 const searchButton = document.querySelector("#search");
 const cardList = document.querySelector(".card-list");
+const cardTitle = document.querySelector("#card-title");
+
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault()
-  //delete previous
+  removeEntries()
   let userPokemon = userInput.value
-  getPokemon(userPokemon)
+  const arr = userPokemon.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const userPokemon2 = arr.join(" ");
+  cardTitle.innerText = `${userPokemon2} Cards`;
+  getPokemon(userPokemon2)
 })
+
+function removeEntries() {
+  cardList.innerHTML = ''
+}
 
 function renderPokemon(pokemonData) {
   pokemonData.forEach((pokemon) => {
     console.log(pokemon)
     const cardPic = document.createElement("img");
     //     const cardYear = document.createElement("h4");
-    //     // listMovies.createElement("div")
+    //     // cardList.createElement("div")
 
     cardPic.src = pokemon.images.small;
     cardList.appendChild(cardPic);
 
     // cardYear.innerText = pokemon.Year;
-    // listMovies.appendChild(cardYear);
+    // cardList.appendChild(cardYear);
 
   }
   )
